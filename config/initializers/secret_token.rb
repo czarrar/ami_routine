51518546baf4cb4a545fdcd94b807cf5341214a0
Zarrar Shehzad <czarrar@gmail.com>
2012-06-28 18:29:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmiRoutine::Application.config.secret_token = 'c6277c643216eee0b8ee4969f42db93ec313b9ac7c9761fad3d0094dd025fb5548f67618ff9e91cc289acae6cf358169d49bb1e4388eccf81f2b4e34faa48b5f'
