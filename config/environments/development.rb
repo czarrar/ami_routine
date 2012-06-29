dd22ef4aed1d7690153f45839ab9bee6b87f5960
Zarrar Shehzad <czarrar@gmail.com>
2012-06-29 11:25:12 -0400

    Removed old gmail plugin; Setup gmail settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,16 @@ AmiRoutine::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "localhost.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+  
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,16 @@ AmiRoutine::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "amiroutine.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+  
   
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
