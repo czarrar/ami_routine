aa41f958f1c4e44530fa7eff8bd10147814679af
Zarrar Shehzad <czarrar@gmail.com>
2012-08-26 00:06:41 -0400

    New homepage for parents with most recent routines and photos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,36 @@
 class HomeController < ApplicationController
   def index
+    if current_user.has_role? :parent
+      # Get routines completed on the most recent day
+      child_names = current_user.children
+      @children = child_names.collect do |child|
+        routines = child.routines.published?
+        routines = routines.order('starts_at DESC')
+        # keep all routines from the latest day
+        latest_routine = routines.limit(1).first
+        if latest_routine
+          requested_day = latest_routine.ends_at.midnight
+          routines = routines.range_for_day(requested_day)
+        else
+          requested_day = Time.now.midnight
+          routines = []
+        end
+        
+        has_album = child.album_id.nil? || child.album_key.nil? ? false : true
+        if has_album
+          smug = get_smugmug
+          album = Smile::Album.find(:album_id => child.album_id, :album_key => child.album_key)
+          photos = album.photos
+          # keep the 5 most recent photos
+          i = photos.length > 4 ? photos.length - 5 : 0
+          photos = photos[i..-1]
+        else
+          photos = []
+        end
+        
+        {name: child.name, date: requested_day, routines: routines, photos: photos}
+      end
+    end
   end
 
   def contact
