c2362ba4104e2b0169e27619da01709161869bd7
Zarrar Shehzad <czarrar@gmail.com>
2012-09-01 14:44:53 -0400

    google oauth fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -207,7 +207,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-  config.omniauth :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {access_type: 'online', approval_prompt: ''}
+  # config.omniauth :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {access_type: 'online', approval_prompt: ''}
+  config.omniauth :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {access_type: 'online', approval_prompt: ''}
   
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
